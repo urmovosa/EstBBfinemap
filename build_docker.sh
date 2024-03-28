f180eb2b6af3c54f62780435c77b9b59e782a1ad
Urmo Võsa <urmo.vosa@gmail.com>
2024-03-28 15:57:49 +0200

    Update docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 sudo docker build -t quay.io/urmovosa/estbbfinemap:v0.1 .
 
 ### Push to quay.io
-sudo docker push quay.io/urmovosa/estbbfinemap:v0.1
+#sudo docker push quay.io/urmovosa/estbbfinemap:v0.1
